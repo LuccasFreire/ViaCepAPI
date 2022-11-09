cda3ff57cb411572ac08a18f5dd12ce7505b1dcc
Lucas Freire <lucasfreiredev1@gmail.com>
2022-11-08 22:35:37 -0300

    Created entitity class Addres created attributes and get and setters

---
+++ ShowAddressViaCep/src/entities/Address.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class Address {
+	
+	private String street;
+	private String nbhd;
+	private String city;
+	private String uf;
+	
+	
+	@Override
+	public String toString() {
+		return "Address [Street = " + street + ", Neighbourhood = " + nbhd + ", City = " + city + ", UF = " + uf + "]";
+	}
+
+	public String getStreet() {
+		return street;
+	}
+	
+	public String getNbhd() {
+		return nbhd;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public String getUf() {
+		return uf;
+	}
+	
+	
+}
