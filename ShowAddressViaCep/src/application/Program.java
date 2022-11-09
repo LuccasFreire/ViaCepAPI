26b04632e39681332f530620a6d913078e4bd510
Lucas Freire <lucasfreiredev1@gmail.com>
2022-11-08 23:18:45 -0300

    Created main class and some codes

---
+++ ShowAddressViaCep/src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
+		String cep;
+		System.out.println("Insert the cep of your street: ");
+		cep = kb.nextLine();
+		
+		
+		
+	}
+
+}
