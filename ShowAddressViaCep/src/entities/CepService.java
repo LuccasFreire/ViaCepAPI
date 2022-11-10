9c60aa154d9fa4a2b9b7990cb485c8e6b67ff749
Lucas Freire <lucasfreiredev1@gmail.com>
2022-11-09 23:19:55 -0300

    Created CepService class and imports

---
+++ ShowAddressViaCep/src/entities/CepService.java
@@ -0,0 +1,11 @@
+package entities;
+import com.google.gson.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class CepService {
+	
+	
+}
