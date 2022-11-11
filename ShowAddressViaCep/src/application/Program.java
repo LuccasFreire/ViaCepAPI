b1e8a6ce3757127d38215ea5cd2c7f48c86872e9
Lucas Freire <lucasfreiredev1@gmail.com>
2022-11-10 22:54:28 -0300

    class CepService completed the code

--- ShowAddressViaCep/src/application/Program.java
+++ ShowAddressViaCep/src/application/Program.java
@@ -6,13 +6,14 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner kb = new Scanner(System.in);
-		String cep;
+		
 		System.out.println("Insert the cep of your street: ");
-		cep = kb.nextLine();
+		String cep = kb.nextLine();
 		
 		
-		
+		kb.close();
 	}
 
 }
--- ShowAddressViaCep/src/entities/CepService.java
+++ ShowAddressViaCep/src/entities/CepService.java
@@ -7,7 +7,27 @@ import java.net.URL;
 
 public class CepService {
 	static String webService = "http://viacep.com.br/ws/";
-	static int Sucess = 200;
+	static int success = 200;
 	
+	public static Address buscaEnderecoPelo(String cep) throws Exception {
+		String apiRequest = webService + cep + "/json";
+		try {
+			URL url = new URL(apiRequest);
+			HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+			
+			if (conexao.getResponseCode() != success) {
+				throw new RuntimeException("HTTP error code: " + conexao.getResponseCode());
+				
+				BufferedReader resposta = new BufferedReader(new InputStreamReader((conexao.getInputStream())));
+				//String jsonEmString = Util.converteJsonEmString(resposta);
+				Gson gson = new Gson();
+	            //Endereco endereco = gson.fromJson(jsonEmString, Endereco.class);
+				return Endereco;
+			}
+		}
+		catch (Exception e){
+			throw new Exception("ERRO: " + e);
+		}
+	}
 	
 }
